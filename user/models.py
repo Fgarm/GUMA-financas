f7caece6817806a7513b2cdb03db47e5908fcf98
EdgarVeider <guimatos.com@gmail.com>
2023-04-04 20:53:20 -0300

    creat user table and connect to database mysql

--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255)
+    email = models.EmailField(primary_key=True)
+    senha = models.CharField(max_length=500)   #pode estourar de acordo com a hash, analisar
+    
