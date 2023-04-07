6100f375b591a907ed2d473015b87c3107102450
Guilherme <69016293+Fgarm@users.noreply.github.com>
2023-04-07 20:06:24 -0300

    Delete unused class

--- BACK/user/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Usuario(models.Model):
-    nome = models.CharField(max_length=255)
-    sobrenome = models.CharField(max_length=255)
-    email = models.EmailField(primary_key=True)
-    senha = models.CharField(max_length=500)  # Grande para guardar hash da senha
-    
