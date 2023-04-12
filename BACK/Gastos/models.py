3da9947e3091c76f720e97093c9c6ca69f7404cc
Gustavo Martins <gtavomartins893@gmail.com>
2023-04-12 11:03:56 -0300

    chore: create Meta class in Gasto models

--- BACK/Gastos/models.py
+++ BACK/Gastos/models.py
@@ -5,3 +5,10 @@ class Gasto(models.Model):
     valor = models.DecimalField(max_digits=9, decimal_places=2) # vi que DecimalField pode ser bom para trabalhar com dinheiro, discutir  
     data = models.DateField()
     pago = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Gasto"
+        verbose_name_plural = "Gastos"
+
+        def __str__(self):
+            return self.nome
