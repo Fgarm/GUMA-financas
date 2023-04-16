25ecf1856017667cf8034a24c67f081b7abb4072
EdgarVeider <guimatos.com@gmail.com>
2023-04-16 18:44:51 -0300

    conexão com frontend de login partes

--- BACK/Usuario/views.py
+++ BACK/Usuario/views.py
@@ -3,17 +3,23 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as login_django
+from rest_framework.decorators import api_view
+from django.views.decorators.csrf import csrf_exempt
 
-
+@csrf_exempt
+@api_view(['GET','POST'])
 def cadastro (request):
+    print()
+    print(request.data)
+    print()
     if request.method == "GET":
         return render(request, 'cadastro.html')
     else:
-        username = request.POST.get('username')
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        email = request.POST.get('email')
-        senha = request.POST.get('senha')
+        username = request.data['username']
+        first_name = request.data['first_name']
+        last_name = request.data['last_name']
+        email = request.data['email']
+        senha = request.data['senha']
     
         user = User.objects.filter(username=username).first()
 
@@ -32,22 +38,27 @@ def cadastro (request):
 
         return HttpResponse('Usuario cadastrado com sucesso')
     
-
+@csrf_exempt
+@api_view(['GET','POST'])
 def login(request):
     if request.method == "GET":
         return render(request, 'login.html')
     else:
-        username = request.POST.get('username')
-        senha = request.POST.get('senha')
+        username = request.data['username']
+        senha = request.data['senha']
+        print(request.data)
 
         user = authenticate(username=username, password=senha)
-
+        print(user)
         if user:
             login_django(request, user)
+            print("atenticado")
             return HttpResponse("autenticado")
         else:
+            print("nao atenticado")
             return HttpResponse("Email ou senha invalido")
-        
+
+
 def vips(request):
     if request.user.is_authenticated:
         return HttpResponse('Entrou na area VIP')
