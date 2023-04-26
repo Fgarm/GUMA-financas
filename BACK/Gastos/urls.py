8c5e2a177a4e1ca860b7025943168b3b1988f13d
Fgarm <guilhermear.maturana@outlook.com>
2023-04-26 15:46:14 -0300

    Changed update gastos and get gastos by tag

--- BACK/Gastos/urls.py
+++ BACK/Gastos/urls.py
@@ -4,6 +4,7 @@ from .views import GastoApiView
 
 urlpatterns = [
     path('meus-gastos/', GastoApiView.get_gastos, name='meus-gastos'),
+    path('gastos-per-tag/', GastoApiView.pegar_gasto_tag, name='gastos-da-tag'),
     path('criar-gasto/', GastoApiView.post_gastos, name='criar-gasto'),
     path('atualizar-gasto/', GastoApiView.put_gasto, name='atualizar-gasto'),
     path('deletar-gasto/', GastoApiView.delete_gasto, name='deletar-gasto'),
