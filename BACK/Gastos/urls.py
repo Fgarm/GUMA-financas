1ddb7677ed9c4770ee32d50df7b772104a90bc0c
Gustavo Martins <gtavomartins893@gmail.com>
2023-04-26 17:11:30 -0300

    feat: create filter by gasto pago or nao pago

--- BACK/Gastos/urls.py
+++ BACK/Gastos/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('criar-gasto/', GastoApiView.post_gastos, name='criar-gasto'),
     path('atualizar-gasto/', GastoApiView.put_gasto, name='atualizar-gasto'),
     path('deletar-gasto/', GastoApiView.delete_gasto, name='deletar-gasto'),
-    path('obter-gasto/', GastoApiView.get_gasto_username, name='obter-gasto')
+    path('obter-gasto/', GastoApiView.get_gasto_username, name='obter-gasto'),
+    path('filtrar-por-pago/', GastoApiView.get_gasto_filter_pago, name='filtrar-por-pago')
 ]
