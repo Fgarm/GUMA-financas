4f99b58caedf7645e4bd830c1b6431eaa214cc61
Fgarm <guilhermear.maturana@outlook.com>
2023-04-28 03:16:28 -0300

    Housekeeping

--- BACK/Gastos/views.py
+++ BACK/Gastos/views.py
@@ -24,13 +24,17 @@ class GastoApiView(APIView):
     @api_view(['POST'])
     def pegar_gasto_tag(request):
         if request.method == 'POST':
-            user_id = User.objects.filter(username=request.data["user"]).first()
-            if not user_id:
-                return Response("sem user", status=HTTPStatus.BAD_REQUEST)
-            tag = Tag.objects.filter(user=user_id.id, categoria=request.data["tag"]).first()
-            if not tag:
-                return Response("sem tag", status=HTTPStatus.BAD_REQUEST)
-            gasto = Gasto.objects.filter(user=user_id.id, tag=tag)
+            try:
+                user = User.objects.get(username=request.data["user"])
+            except User.DoesNotExist:
+                return Response("Username incorreto ou inexistente", status=status.HTTP_404_NOT_FOUND)
+            try:
+                tag = Tag.objects.get(user=user.username, categoria=request.data["tag"])
+            except Tag.DoesNotExist:
+                return Response("Não há essa tag", status=HTTPStatus.BAD_REQUEST)
+            except Tag.MultipleObjectsReturned:
+                return Response("Há muitas tags com mesmo user e name", status=HTTPStatus.BAD_REQUEST)
+            gasto = Gasto.objects.filter(user=user.username, tag=tag.categoria)
             serializer = GastoSerializer(gasto, context={'request': request}, many=True)
             return Response(serializer.data, status=HTTPStatus.ACCEPTED)
 
@@ -38,16 +42,14 @@ class GastoApiView(APIView):
     def get_gasto_username(request):
         #print("parametros?:", request.query_params)
         #print("dados?:", request.data)
-        username = request.data["user"]
-        user_id = User.objects.filter(username=username).first()
-        #Verificando se User existe
-        if not user_id:
-            return Response("user errado", status=status.HTTP_404_NOT_FOUND)
-        else:
-            user_id = user_id.id
-
-        gastos = Gasto.objects.filter(user_id=user_id)
-        #Virificando de possui gastos
+        try:
+            user = User.objects.get(username=request.data["user"])
+        except User.DoesNotExist:
+            return Response("Username incorreto ou inexistente", status=status.HTTP_404_NOT_FOUND)
+        except User.MultipleObjectsReturned:
+            return Response("Há muitos usuários com msm username", status=HTTPStatus.BAD_REQUEST)
+        
+        gastos = Gasto.objects.filter(user=user.username)
 
         serializer = GastoSerializer(gastos, context={'request': request}, many=True)
         return Response(serializer.data)
@@ -60,27 +62,30 @@ class GastoApiView(APIView):
             data["valor"] = request.data["valor"]
             data["data"] = request.data["data"]
             data["pago"] = request.data["pago"]
-            print("0:  ", data)
 
-            user_id = User.objects.filter(username=request.data["user"]).first()
-            if not user_id: 
-                return Response("user errado", status=status.HTTP_400_BAD_REQUEST)
-
-            user_id = user_id.id
-            data["user"] = user_id
             try:
-                tag_id = Tag.objects.filter(categoria=request.data["tag"]).filter(user=data["user"]).first()
-                if tag_id:
-                    data["tag"] = tag_id.id
-            except KeyError:
-                pass
-                #return Response("n tem essa tag", status=status.HTTP_400_BAD_REQUEST)
-            print("1:  ",data)
+                user = User.objects.get(username=request.data["user"])
+                data["user"] = user.username
+            except User.DoesNotExist:
+                return Response("Username incorreto ou inexistente", status=status.HTTP_404_NOT_FOUND)
+            except User.MultipleObjectsReturned:
+                return Response("Há muitos usuários com msm username", status=HTTPStatus.BAD_REQUEST)
+            
+            if "tag" in request.data:
+                try:
+                    tag = Tag.objects.get(categoria=request.data["tag"], user=user.username)
+                    data["tag"] = tag.categoria
+                except Tag.DoesNotExist:
+                    #return Response("Não há essa tag", status=HTTPStatus.BAD_REQUEST)
+                    #TODO: descomentar esse code depois da apresentação do Hubner
+                    pass
+                except Tag.MultipleObjectsReturned:
+                    return Response("Há muitas tags com mesmo user e name", status=HTTPStatus.BAD_REQUEST)
+            
             serializer = GastoSerializer(data=data, context={'request': request})
 
             if serializer.is_valid():
                 serializer.save()
-
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -88,9 +93,7 @@ class GastoApiView(APIView):
     @api_view(['PUT'])
     def put_gasto (request):
         try:
-            #print(request.data["id"])
-            reqId = request.data["id"]
-            gasto = Gasto.objects.get(id=reqId)
+            gasto = Gasto.objects.get(id=request.data["id"])
         except Gasto.DoesNotExist:
             return Response("Não há gasto com esse id", status=status.HTTP_404_NOT_FOUND)
         data = {}
@@ -98,10 +101,22 @@ class GastoApiView(APIView):
         data["valor"] = request.data["valor"]
         data["data"] = request.data["data"]
         data["pago"] = request.data["pago"]
-        tag = Tag.objects.filter(user=gasto.user, categoria=request.data["tag"]).first()
-        if tag:
-            data["tag"] = tag.id
-        
+        data["tag"] = gasto.tag
+        if "tag" in request.data:
+            try:
+                if request.data["tag"] == "":
+                    data["tag"] = None
+                elif request.data["tag"] is None:
+                    data["tag"] = None
+                else:
+                    tag = Tag.objects.get(categoria=request.data["tag"], user=gasto.user)
+                    data["tag"] = tag.categoria
+            except Tag.DoesNotExist:
+                #return Response("Não há essa tag", status=HTTPStatus.BAD_REQUEST)
+                #TODO: descomentar esse code depois da apresentação do Hubner
+                pass
+            except Tag.MultipleObjectsReturned:
+                return Response("Há muitas tags com mesmo user e name", status=HTTPStatus.BAD_REQUEST)
 
         if request.method == 'PUT':
             serializer = GastoSerializer(gasto, data=data, context={'request': request})
@@ -113,15 +128,13 @@ class GastoApiView(APIView):
     @api_view(['DELETE'])
     def delete_gasto (request):
         try:
-            print(request.data["id"])
-            reqId = request.data["id"]
-            gasto = Gasto.objects.get(id=reqId)
+            gasto = Gasto.objects.get(id=request.data["id"])
         except Gasto.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
         
         if request.method == 'DELETE':
             gasto.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response("Deletado com sucesso", status=status.HTTP_204_NO_CONTENT)
         
 
     @api_view(['GET', 'POST'])
@@ -132,16 +145,17 @@ class GastoApiView(APIView):
         # print(request.data)
 
         # obtendo o user selecionado
-        username = request.data["user"]
-        user_id = User.objects.filter(username=username).first()
-
+        try:
+            user = User.objects.get(username=request.data["user"])
         # verificando se o user selecionado existe
-        if not user_id:
+        except User.DoesNotExist:
             return Response("Username incorreto ou inexistente", status=status.HTTP_404_NOT_FOUND)
+        except User.MultipleObjectsReturned:
+            return Response("Há muitos usuários com msm username", status=HTTPStatus.BAD_REQUEST)
+
 
         # Obtendo todos os gastos do usuario selecionado
-        user_id = user_id.id
-        gastos = Gasto.objects.filter(user_id=user_id)
+        gastos = Gasto.objects.filter(user=user.username)
 
         # verificando se o user tem algum gasto
         if not gastos:
