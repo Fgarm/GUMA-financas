418ee23903778d69daceacb9581442512aac1ecc
Gustavo Martins <gtavomartins893@gmail.com>
2023-05-09 09:27:50 -0300

    chore: changes in chart component

--- FRONT/src/pages/Chart/index.jsx
+++ FRONT/src/pages/Chart/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useContext } from "react";
 import './style.css';
 
 import { UserData } from "./Data";
@@ -13,24 +13,28 @@ import axios from 'axios';
 
 export default function ChartComponent() {
 
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.year),
-    datasets: [
-      {
-        label: "Users Gained",
-        data: UserData.map((data) => data.userGain),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f6f3b3",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-  });
+  const [isLoading, setIsLoading] = useState(true);
+  const [userData, setUserData] = useState(null);
+
+
+  // const [userData, setUserData] = useState({
+  //   labels: UserData.map((data) => data.year),
+  //   datasets: [
+  //     {
+  //       label: "Users Gained",
+  //       data: UserData.map((data) => data.userGain),
+  //       backgroundColor: [
+  //         "rgba(75,192,192,1)",
+  //         "#ecf0f1",
+  //         "#50AF95",
+  //         "#f6f3b3",
+  //         "#2a71d0",
+  //       ],
+  //       borderColor: "black",
+  //       borderWidth: 2,
+  //     },
+  //   ],
+  // });
 
   useEffect(() => {
     setUserData({
@@ -51,15 +55,18 @@ export default function ChartComponent() {
         },
       ],
     });
+    setIsLoading(false);
   }, []);
+
+  // console.log(userData)
 
   return (
     <div className="ChartComponent">
       <div style={{ width: 700 }}>
-        {userData.labels && userData.datasets ? (
-          <Bar chartData={userData} />
-        ) : (
+        {isLoading ? (
           <p>Loading chart data...</p>
+          ) : (
+          <Bar chartData={userData} />
         )}
       </div>
     </div>
