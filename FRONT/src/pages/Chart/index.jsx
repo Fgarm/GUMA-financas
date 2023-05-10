d37f3d53cde3891dca2e5bbc8acb42e78e0be6a7
Gustavo Martins <gtavomartins893@gmail.com>
2023-05-10 01:58:52 -0300

    feat: created Line and Donut chats components

--- FRONT/src/pages/Chart/index.jsx
+++ FRONT/src/pages/Chart/index.jsx
@@ -1,74 +1,118 @@
-import React, { useState, useEffect, useRef, useContext } from "react";
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+  ArcElement, // p grafico de rosquinha
+} from 'chart.js';
+import { Line, Doughnut } from 'react-chartjs-2';
+
 import './style.css';
+import faker from 'faker';
 
-import { UserData } from "./Data";
-import { Bar } from "react-chartjs-2";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+  ArcElement // p grafico de rosquinha
+);
 
-import { Input, InputGroup, InputRightElement, Button, Link } from '@chakra-ui/react';
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+export const options1 = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Extrato dos últimos 6 meses',
+    },
+  },
+};
 
+export const options2 = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Gastos mais Relevantes de Janeiro',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      id: 1,
+      fill: true,
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: -500, max: 1000 })),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgb(53, 162, 235, 0.3)',
+    },
+  ],
+};
+
+export const data2 = {
+  labels: ['Mercado', 'Aluguel', 'Energia', 'Água', 'Internet', 'Transporte'],
+  datasets: [
+    {
+      label: 'R$',
+      data: [350, 220, 40, 35, 50, 100],
+      backgroundColor: [
+        'rgba(54, 162, 235, 0.2)',  // Blue
+        'rgba(75, 192, 192, 0.2)',  // Green
+        'rgba(255, 206, 86, 0.2)',  // Yellow
+        'rgba(255, 159, 64, 0.2)', // Orange
+        'rgba(255, 99, 132, 0.2)',  // Red
+        'rgba(153, 102, 255, 0.2)', // Purple
+      ],
+      borderColor: [
+        'rgba(54, 162, 235, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(153, 102, 255, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+
+// export const data2 = data.datasets.find((dataset) => dataset.id == 2);
 
 
 export default function ChartComponent() {
-
-  const [isLoading, setIsLoading] = useState(true);
-  const [userData, setUserData] = useState(null);
-
-
-  // const [userData, setUserData] = useState({
-  //   labels: UserData.map((data) => data.year),
-  //   datasets: [
-  //     {
-  //       label: "Users Gained",
-  //       data: UserData.map((data) => data.userGain),
-  //       backgroundColor: [
-  //         "rgba(75,192,192,1)",
-  //         "#ecf0f1",
-  //         "#50AF95",
-  //         "#f6f3b3",
-  //         "#2a71d0",
-  //       ],
-  //       borderColor: "black",
-  //       borderWidth: 2,
-  //     },
-  //   ],
-  // });
-
-  useEffect(() => {
-    setUserData({
-      labels: UserData.map((data) => data.year),
-      datasets: [
-        {
-          label: "Users Gained",
-          data: UserData.map((data) => data.userGain),
-          backgroundColor: [
-            "rgba(75,192,192,1)",
-            "#ecf0f1",
-            "#50AF95",
-            "#f6f3b3",
-            "#2a71d0",
-          ],
-          borderColor: "black",
-          borderWidth: 2,
-        },
-      ],
-    });
-    setIsLoading(false);
-  }, []);
-
-  // console.log(userData)
-
   return (
-    <div className="ChartComponent">
-      <div style={{ width: 700 }}>
-        {isLoading ? (
-          <p>Loading chart data...</p>
-          ) : (
-          <Bar chartData={userData} />
-        )}
+    <div className="container">
+      
+      <div className="LineChartComponent">
+        <Line datasetIdKey='id' options={options1} data={data} />
       </div>
+      
+      <div className="DonutChartComponent">
+        <Doughnut options={options2} data={data2} />
+      </div>
+    
     </div>
   );
 }
