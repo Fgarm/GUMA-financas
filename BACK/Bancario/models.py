3687f36d58d7a898ee9779c9c33b7d318622fdc7
EdgarVeider <guimatos.com@gmail.com>
2023-05-23 00:16:51 -0300

    Saldos criados

--- BACK/Bancario/models.py
+++ BACK/Bancario/models.py
@@ -1,3 +1,14 @@
+from uuid import uuid4
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Bancario(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    id_usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    saldo_atual = models.DecimalField(max_digits=20, decimal_places=2)
+
+class Saldos(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    id_grupo = models.ForeignKey(Bancario, on_delete=models.CASCADE, null=True)
+    saldo = models.DecimalField(max_digits=20, decimal_places=2)
+    data = models.IntegerField()
--- BACK/GUMA_investimentos/urls.py
+++ BACK/GUMA_investimentos/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-# from Gastos.views import GastoApiView
+from django.conf.urls.static import static
+from django.conf import settings
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -9,16 +10,19 @@ urlpatterns = [
 
 
 
-    #Usuario rotas
+    # Rotas Usuario
     path('auth/', include('Usuario.urls')),
     path('token/', TokenObtainPairView.as_view()),
     path('token/refresh/', TokenRefreshView.as_view()),
 
 
-    # API de Gastos
+    # Rotas Gastos
     path('api/gastos/', include('Gastos.urls')),
     
-    # Caminho Tags
+    # Rotas Tags
     path('tags/', include('Tags.urls')),
 
-]
+    # Rotas Grupo
+    path('grupos/', include('Grupos.urls'))
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
