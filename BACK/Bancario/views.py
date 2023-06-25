16b2acb33b811e07cf9f6a4a66c4c0b705932204
Gustavo Martins <gtavomartins893@gmail.com>
2023-06-25 17:15:15 -0300

    chore: firsts steps to create view to get previous monthly balances

--- BACK/Bancario/views.py
+++ BACK/Bancario/views.py
@@ -8,7 +8,6 @@ from Gastos.models import Gasto
 from rest_framework import status
 import datetime
 
-
 class BancarioView(APIView):
     @api_view(['POST'])
     def add_saldo(request):
@@ -75,6 +74,46 @@ class BancarioView(APIView):
 
         return Response(saldo ,status=status.HTTP_200_OK)
     
+    @api_view(['GET', 'POST'])
+    def get_saldo_meses_anteriores(request):
 
+        # print("request: ", request.data)
+        # print("user: ", request.data["user"])
 
+        # obtendo os gastos do user selecionado
+        try:
+            gastos = Gasto.objects.filter(user=request.data["user"])
+            print("gastos", gastos)
+            
+        # verificando se o user selecionado existe
+        except Gasto.DoesNotExist:
+            return Response("Username incorreto ou inexistente ou o usuário não tem nenhum gasto", status=status.HTTP_404_NOT_FOUND)
         
+        meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+        data = []
+        labels = []
+
+        # obtendo o mês e o ano atuais
+        mesAtual = datetime.now().month + 1
+        anoAtual = datetime.now().year
+
+        # somando todos os gastos de cada mês durante os 12 meses anteriores
+        for i in range(12):
+
+            mesAtual -= 1
+            if mesAtual == 0:
+                mesAtual = 12
+                anoAtual -= 1
+
+            # soma todos os gastos do mês/ano daquela iteração
+            somaTodosGastosMes = np.sum([gasto.valor for gasto in gastos if gasto.data.month == mesAtual and gasto.data.year == anoAtual and gasto.pago == True]) # incluir apenas gastos pagos? discutir
+            
+            # adicionando os meses na lista de labels que será usada no gráfico
+            labels.append(meses[mesAtual-1])
+
+            # adicionando a soma dos gastos de cada mês na lista de dados que será exibida no gráfico
+            data.append(somaTodosGastosMes)
+
+        # inverte ambas listas para ficar da forma correta no gráfico
+        json_response = {'data': data[::-1], 'labels': labels[::-1]}
+        return Response(json_response, status=status.HTTP_200_OK)
