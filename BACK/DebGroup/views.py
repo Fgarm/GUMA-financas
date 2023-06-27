da2a83867f0c6a9566fe144470dc91c6af337913
Guilherme Matos <guimatos.com@gmail.com>
2023-06-27 00:50:17 -0300

    save changes

--- BACK/DebGroup/views.py
+++ BACK/DebGroup/views.py
@@ -1,21 +1,24 @@
 from Grupos.models import Gastos_Grupo
 from DebGroup.models import Debitos
-
+from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from django.contrib.auth.models import User
 
 
-class AuxFunctions(APIView):
-    def criar_debito(provedor, grupo_gasto, item, valor):
-        devedor = Debitos.objects.filter(grupoGasto_id=grupo_gasto).first().grupoGasto_id
-        debito = Debitos.objects.create(provedor=provedor,
-                                        devedor=devedor, 
-                                        grupo_gasto=grupo_gasto, 
-                                        valor=valor, 
-                                        item=item)
+class AuxDebitos(APIView):
+    def criar_debito(valor, devedor, provedor, item, grupo_gasto_id):
+        
+        debito = Debitos.objects.create(valor=valor, devedor_id=devedor, provedor_id=provedor,
+                                        item_id=item, grupo_gasto_id=grupo_gasto_id)
+        
 
+class DebGroupView(APIView):
+    
+    @api_view(['POST'])
+    def user_dividas(request):
+        user_dividas = Debitos.objects.filter(grupo_gasto_id=request.data["grupo_gasto_id"])
+        user_id = User.objects.filter(username=request.data["username"])
 
-
-
-
-
-
+        
+        for divida in user_dividas:
+            if user_id == divida.devedor:
