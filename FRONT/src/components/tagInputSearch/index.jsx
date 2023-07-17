584e79aa9f606cf5162d63a425a5c4fba33c1062
Thiago Gariani <thiagogarianiq@gmail.com>
2023-07-17 11:30:18 -0300

    commit

--- FRONT/src/components/tagInputSearch/index.jsx
+++ FRONT/src/components/tagInputSearch/index.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { GetTags, GetGastos } from "../../services/users";
 
 export default function TagsInputSearch(props) {
 
